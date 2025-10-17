964dce52f4695fcc435603c8ccddb8304626761a
vietdan1613 <viet.dan.1613@gmail.com>
2025-10-17 10:29:34 +0700

    update react peformance

--- src/begins/01.element-optimization/Box3.tsx
+++ src/begins/01.element-optimization/Box3.tsx
@@ -1,3 +1,45 @@
+import React, { useState } from "react";
+
+function GrandchildBox({ text }: { text: string }) {
+    console.log("Render GrandchildBox");
+    return <div className="bg-yellow-500 rounded mt-2 px-3 py-1 text-white">{text}</div>;
+}
+
+function ChildBox({ grandchild }: { grandchild: React.ReactNode }) {
+    console.log("Render ChildBox");
+    const [count, setCount] = useState(0);
+
+    return <div className="bg-red-500 rounded mt-2 px-3 py-4 text-white">
+        <p>ChildBox</p>
+        <button
+            className="mt-2 px-3 py-1 bg-blue-500 text-white rounded-lg"
+            onClick={() => setCount(count + 1)}
+        >
+            Count is {count}
+        </button>
+        {grandchild}
+    </div>;
+}
+
 export function Box3() {
-    return <div className="p-4 bg-gray-100 rounded-xl shadow-sm">Box 3</div>;
+    const [childText, setChildText] = useState("GrandchildBox Text");
+    const [count, setCount] = useState(0);
+    console.log("Render Box1");
+    return (
+        <div className="p-4 bg-green-500 rounded-xl shadow-sm">
+            <div className="font-bold">Box 3 <span className="text-sm font-normal">{childText}</span></div>
+            <button
+                className="mt-2 px-3 py-1 bg-blue-500 text-white rounded-lg"
+                onClick={() => setCount(count + 1)}
+            >
+                Count is {count}
+            </button>
+            <input
+                className="mt-2 px-3 py-1 rounded-lg border"
+                value={childText}
+                onChange={(e) => setChildText(e.target.value)}
+            />
+            <ChildBox grandchild={<GrandchildBox text={childText} />} />
+        </div>
+    )
 }

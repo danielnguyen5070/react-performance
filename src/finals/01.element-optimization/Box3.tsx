f663701d91d71836c07e2b292fa0fc60d5c46113
vietdan1613 <viet.dan.1613@gmail.com>
2025-10-17 10:42:22 +0700

    update react peformance

--- src/finals/01.element-optimization/Box3.tsx
+++ src/finals/01.element-optimization/Box3.tsx
@@ -1,3 +1,59 @@
+import React, { useState } from "react";
+
+function GrandchildBox() {
+    const text = useTextChild();
+    console.log("Render GrandchildBox");
+    return <div className="bg-yellow-500 rounded mt-2 px-3 py-1 text-white">{text}</div>;
+}
+
+function ChildBox({ grandchild }: { grandchild: React.ReactNode }) {
+    console.log("Render ChildBox");
+    const [count, setCount] = useState(0);
+
+    return <div className="bg-red-500 rounded mt-2 px-3 py-4 text-white">
+        <p>ChildBox</p>
+        <button
+            className="mt-2 px-3 py-1 bg-blue-500 text-white rounded-lg"
+            onClick={() => setCount(count + 1)}
+        >
+            Count is {count}
+        </button>
+        {grandchild}
+    </div>;
+}
+
+const grandchildBoxElement = <GrandchildBox />;
 export function Box3() {
-    return <div className="p-4 bg-gray-100 rounded-xl shadow-sm">Box 3</div>;
+    const [text, setText] = useState("GrandchildBox Text");
+    const [count, setCount] = useState(0);
+    console.log("Render Box1");
+    return (
+        <TextChildContext value={text}>
+            <div className="p-4 bg-green-500 rounded-xl shadow-sm">
+                <div className="font-bold">Box 3 <span className="text-sm font-normal">{text}</span></div>
+                <button
+                    className="mt-2 px-3 py-1 bg-blue-500 text-white rounded-lg"
+                    onClick={() => setCount(count + 1)}
+                >
+                    Count is {count}
+                </button>
+                <input
+                    className="mt-2 px-3 py-1 rounded-lg border"
+                    value={text}
+                    onChange={(e) => setText(e.target.value)}
+                />
+                <ChildBox grandchild={grandchildBoxElement} />
+            </div>
+        </TextChildContext>
+    )
+}
+
+const TextChildContext = React.createContext<string | null>(null);
+
+function useTextChild() {
+    const context = React.useContext(TextChildContext);
+    if (!context) {
+        throw new Error("useTextChild must be used within a TextChildProvider");
+    }
+    return context;
 }

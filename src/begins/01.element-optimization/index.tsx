67420a8d88d32f9d5b5f2a23cb666cd9a901a4c9
vietdan1613 <viet.dan.1613@gmail.com>
2025-10-17 12:59:04 +0700

    update react performance

--- src/begins/01.element-optimization/index.tsx
+++ src/begins/01.element-optimization/index.tsx
@@ -1,9 +1,8 @@
-import { useState } from "react";
 import { Box1 } from "./Box1";
 import { Box2 } from "./Box2";
 import { Box3 } from "./Box3";
-import { Box4 } from "./Box4";
 import { Box5 } from "./Box5";
+import { Box4 } from "./Box4";
 
 function Main({
 	box1,
--- src/finals/01.element-optimization/index.tsx
+++ src/finals/01.element-optimization/index.tsx
@@ -1,9 +1,8 @@
-import { useState } from "react";
 import { Box1 } from "./Box1";
 import { Box2 } from "./Box2";
 import { Box3 } from "./Box3";
-import { Box4 } from "./Box4";
 import { Box5 } from "./Box5";
+import { Box4 } from "./Box4";
 
 function Main({
 	box1,
@@ -18,16 +17,8 @@ function Main({
 	box4: React.ReactNode;
 	box5: React.ReactNode;
 }) {
-	const [count, setCount] = useState(0);
-	const increment = () => setCount((c) => c + 1);
 	return (
 		<main className="space-y-6 mt-6">
-			<button
-				onClick={increment}
-				className="px-4 py-2 bg-indigo-500 text-white rounded-lg hover:bg-indigo-600 transition"
-			>
-				The count is {count}
-			</button>
 			<div className="grid grid-cols-2 gap-4">{[box1, box2, box3, box4, box5]}</div>
 		</main>
 	);

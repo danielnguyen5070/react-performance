fc9996ba3dbf01653ccb111b0f7c1a3151571bf4
vietdan1613 <viet.dan.1613@gmail.com>
2025-10-18 13:39:26 +0700

    update react performance

--- src/begins/04.code-splitting/index.tsx
+++ src/begins/04.code-splitting/index.tsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import { useState } from 'react'
+import './index.css'
+import Globe from './globe.tsx'
 
-const index = () => {
+function App() {
+	const [showGlobe, setShowGlobe] = useState(false)
+
 	return (
-		<div>index</div>
+		<div
+			style={{
+				display: 'flex',
+				alignItems: 'center',
+				flexDirection: 'column',
+				justifyContent: 'center',
+				height: '100%',
+				padding: '2rem',
+			}}
+		>
+			<label style={{ marginBottom: '1rem' }}>
+				<input
+					type="checkbox"
+					checked={showGlobe}
+					onChange={(e) => setShowGlobe(e.currentTarget.checked)}
+				/>
+				{' show globe'}
+			</label>
+			<div style={{ width: 400, height: 400 }}>
+				{showGlobe ? <Globe /> : null}
+			</div>
+		</div>
 	)
 }
 
-export default index
+export default App
